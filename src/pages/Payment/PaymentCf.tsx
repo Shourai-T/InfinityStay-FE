81605303e000929351ccc39a0aa4336f78da0013
Mtri nèeeeeee <90810549+mtrine@users.noreply.github.com>
2025-09-07 09:38:59 +0700

    update: payment cf

--- src/pages/Payment/PaymentCf.tsx
+++ src/pages/Payment/PaymentCf.tsx
@@ -1,8 +1,11 @@
-export default function PaymentCf (){
-    if(true){
-        return <div>Payment Successful</div>;
-    }
-    else{
-        return <div>Payment Failed</div>;
-    }
+import { useSearchParams } from "react-router-dom";
+import PaymentFailed from "../Booking/PaymentFailed";
+import Confirmation from "../Confirmation";
+
+export default function PaymentCf() {
+  const [searchParams] = useSearchParams();
+  const status = Number(searchParams.get("status"));
+
+  const isSuccess = status === 1;
+  return <>{isSuccess ? <Confirmation /> : <PaymentFailed />}</>;
 }
